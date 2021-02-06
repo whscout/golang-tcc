9b39c614aa3543f93a08db8f0f9cc2105a7f7c29
hwang <1336224109@qq.com>
2021-02-06 23:48:02 +0800

    'update'

--- internal/server1/dao/dao.go
+++ internal/server1/dao/dao.go
@@ -7,13 +7,3 @@ import (
 type Dao struct {
 	db *mysql.DB
 }
-
-func New(opts ...Option) (*Dao, error) {
-	d := &Dao{}
-	for _, opt := range opts {
-		if err := opt.Apply(d); err != nil {
-			return nil, err
-		}
-	}
-	return d, nil
-}
